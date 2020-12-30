df8b21f050263962f5d70f9ff2e0b914cd59223e
Max Ning <maxning24@gmail.com>
2020-12-29 23:54:32 -0500

    backend functionality complete

--- exercise-tracker/backend/models/exercise_model.js
+++ exercise-tracker/backend/models/exercise_model.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema ({
-    username: { type: String, required: true },
-    description: { type: String, required: true },
-    duration: { type: String, required: true },
-    date: { type: Date, required: true },
+const exerciseSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
 }, {
-    timestamps: true,
+  timestamps: true,
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
--- exercise-tracker/backend/server.js
+++ exercise-tracker/backend/server.js
@@ -11,11 +11,18 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
 const connection = mongoose.connection;
 connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-});
+  console.log("MongoDB database connection established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
